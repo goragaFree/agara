71fc76c0b61140556773f381b4b4be97f0fd59a5
goragaFree <gameroketop@gmail.com>
2026-09-13 22:35:08 +0300

    Delete modules directory

--- modules/player/NoRotate.java
+++
@@ -1,9 +0,0 @@
-package moscow.rockstar.systems.modules.modules.player;
-
-import moscow.rockstar.systems.modules.api.ModuleCategory;
-import moscow.rockstar.systems.modules.api.ModuleInfo;
-import moscow.rockstar.systems.modules.impl.BaseModule;
-
-@ModuleInfo(name = "No Rotate", category = ModuleCategory.PLAYER, desc = "modules.descriptions.no_rotate")
-public class NoRotate extends BaseModule {
-}
--- modules/visuals/esp/EspTarget.java
+++
@@ -1,6 +0,0 @@
-package moscow.rockstar.systems.modules.modules.visuals.esp;
-
-import net.minecraft.entity.Entity;
-
-public record EspTarget(Entity entity, EspTargetType type, EspPlayerType playerType, EspItemType itemType) {
-}
