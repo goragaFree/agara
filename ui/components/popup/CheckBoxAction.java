7946109e8b00cb3d7229c3327212dedd4b313c92
goragaFree <gameroketop@gmail.com>
2026-09-13 22:36:31 +0300

    Delete ui directory

--- ui/components/popup/CheckBoxAction.java
+++
@@ -1,5 +0,0 @@
-package moscow.rockstar.ui.components.popup;
-
-public interface CheckBoxAction {
-   void handleAction(boolean var1);
-}
--- ui/components/popup/PopupAction.java
+++
@@ -1,5 +0,0 @@
-package moscow.rockstar.ui.components.popup;
-
-public interface PopupAction {
-   void run(Popup var1);
-}
